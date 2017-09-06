4b4ca5c7da2d4da0bc71076b38ae10bc18e8a978
felix <2319979647@qq.com>
2017-09-06 23:03:46 +0800

    init

---
+++ AKMediaUBox/AKMediaUBox/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AKMediaUBox
+//
+//  Created by Aka on 2017/9/6.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
