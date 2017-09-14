9af612c35aff5db7cb129f848d4752e424243d5f
felix <2319979647@qq.com>
2017-09-14 20:51:10 +0800

    播放音效

---
+++ AKMediaUBox/AKMediaUBox/TestsoundEffect/YDTestSoundEffectViewController.h
@@ -0,0 +1,13 @@
+//
+//  YDTestSoundEffectViewController.h
+//  AKMediaUBox
+//
+//  Created by Aka on 2017/9/14.
+//  Copyright © 2017年 Aka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YDTestSoundEffectViewController : UIViewController
+
+@end
--- AKMediaUBox/AKMediaUBox/ViewController.m
+++ AKMediaUBox/AKMediaUBox/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "YDTestSoundEffectViewController.h"
 
 @interface ViewController ()
 
@@ -17,9 +18,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+//    0
+    [self testSoundEffect];
 
 }
 
+
+- (void)testSoundEffect {
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    [btn setTitle:@"音效" forState:UIControlStateNormal];
+    [btn addTarget:self action:@selector(onSoundEffectClick:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    btn.frame = CGRectMake(0, 100, 100, 30);
+    
+}
+
+- (void)onSoundEffectClick:(id)sender {
+    [self.navigationController pushViewController:[YDTestSoundEffectViewController new] animated:YES];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
